4c6055d8d909acad03c01d0c2922470deecf3f32
GianGougeon <GianGougeon@gmail.com>
2022-07-26 04:11:55 -0300

    typography + home

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import arcaneImage from './../assets/img/arcane-netflix-series-characters.jpg'
+import {BsArrowRight} from 'react-icons/bs'
+
 const Home = () => {
     return (
         <>
@@ -7,18 +9,24 @@ const Home = () => {
                 <h1>Become <b>a legend</b></h1>
                 <div className='home-catalog container'>
                     <div className='row'>
-                        <div className='col-md-6'>
+                        <div className='col-md-7'>
                             <img src={arcaneImage}></img>
                         </div>
-                        <div className='col-md-6'>
-                            <p>
-                                Explore a lot of different merch
+                        <div className='col-md-5'>
+                           <div>
+                           <p>
+                                Explore a lot of different merch <br></br>
                                 of the world of Arcane
                             </p>
-                            <button>Go to the cataloge</button>
+                            <button>Go to the cataloge<BsArrowRight /></button>
+                           </div>
                         </div>
                     </div>
                 </div>
+
+            </section>
+
+            <section className='home-section-clothes'>
 
             </section>
         </>
